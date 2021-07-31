50d1ea9862a7b5398e5ad366a4cbfe459619de47
Eplankton <86543401+Eplankton@users.noreply.github.com>
2021-07-31 12:28:50 +0800

    Add files via upload

---
+++ main.c
@@ -0,0 +1,45 @@
+//From : Eplanlkton  Date : 2021/7/20
+#include <stdio.h>
+#include "vector_calculatior.h"
+
+int main()
+{
+    int key = 1; //User Interface
+
+    for (; key > 0;)
+    {
+        printf("\n\n");
+        printf("                                #######################\n");
+        printf("                                |     # Vector #      |\n");
+        printf("                                |                     |\n");
+        printf("                                |         +        :1 |\n");
+        printf("                                |         -        :2 |\n");
+        printf("                                |         .        :3 |\n");
+        printf("                                |         x        :4 |\n");
+        printf("                                |                     |\n");
+        printf("                                |        Exit      :0 |\n");
+        printf("                                #######################\n\n");
+        printf("                                Please enter : ");
+
+        scanf("%d", &key);
+        switch (key)
+        {
+        case 1:
+            vectorAddition();
+            break;
+        case 2:
+            vectorSubtraction();
+            break;
+        case 3:
+            vectorDotproduct();
+            break;
+        case 4:
+            vectorCrossproduct();
+            break;
+        case 0:
+            break;
+        }
+    }
+
+    return 0;
+}
